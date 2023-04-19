5d65cdfcc7e8d3de6d36b60e77fcf871784bf5f8
meszmers <mileikamarks@gmail.com>
2023-04-19 21:43:51 +0300

    make database migrations and create models for each database table

---
+++ database/migrations/2023_04_19_173715_create_wallets_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->string('id')->unique();
+            $table->bigInteger('user_id');
+            $table->bigInteger('card_name');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('wallet');
+    }
+};
---
+++ database/migrations/2023_04_19_173719_create_currencies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('currency_code');
+        });
+
+        DB::table('currencies')->insert(
+            [
+                'currency_code' => 'EUR'
+            ]
+        );
+        DB::table('currencies')->insert(
+            [
+                'currency_code' => 'USD'
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('currencies');
+    }
+};
